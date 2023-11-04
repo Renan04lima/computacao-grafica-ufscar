00d462f43e4cde006d56ef15e66ae508fbd197ab
renan04lima <renan04lima@outlook.com>
2023-11-04 09:16:03 -0300

    test: ensure mat4 constructor and methods works correctly

--- Atividade02/includes/vec4.h
+++ Atividade02/includes/vec4.h
@@ -4,12 +4,17 @@
 #include "vec3.h"
 
 class vec4 : public vec3 {
-  public:
+public:
     double w;
 
+    // When w is 1, the vector represents a point in 3D space. When w is 0, the vector represents a direction.
     vec4() : vec3(), w(1) {}
     vec4(double x, double y, double z, double w) : vec3(x, y, z), w(w) {}
-    vec4(const vec3& v3) : vec3(v3), w(1) {}
+    vec4(const vec3 &v3) : vec3(v3), w(1) {}
+
+    bool operator==(const vec4 &other) const {
+        return x() == other.x() && y() == other.y() && z() == other.z() && w == other.w;
+    }
 };
 
 #endif
